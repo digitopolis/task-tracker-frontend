180043dcda16bf87a571e7db2896bec89e4c7968
Matt Readout <matt.readout@gmail.com>
2019-07-28 20:23:16 -0400

    create formcontainer and newuserform components

---
+++ src/components/NewUserForm.js
@@ -0,0 +1,14 @@
+import React from 'react'
+// import PropTypes from 'prop-types'
+
+class NewUserForm extends React.Component {
+	render () {
+		return (
+			<div>
+				<h1>New User Form</h1>
+			</div>
+		)
+	}
+}
+
+export default NewUserForm;
---
+++ src/containers/FormContainer.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import NewUserForm from '../components/NewUserForm'
+// import PropTypes from 'prop-types'
+
+const FormContainer = (props) => {
+	return (
+		<NewUserForm />
+	)
+}
+
+export default FormContainer
