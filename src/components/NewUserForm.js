60d17bd77016ce280bc7018c369411df9eb075c5
Matt Readout <matt.readout@gmail.com>
2019-07-31 22:32:48 -0400

    start building new user form

--- src/components/NewUserForm.js
+++ src/components/NewUserForm.js
@@ -1,14 +1,37 @@
 import React from 'react'
-// import PropTypes from 'prop-types'
+import styled from 'styled-components'
 
 class NewUserForm extends React.Component {
 	render () {
 		return (
-			<div>
-				<h1>New User Form</h1>
-			</div>
+			<OuterDiv >
+				<InnerDiv>
+					<h1>New User Form</h1>
+					<form>
+						Name: <input type='text'/><br/>
+						Username: <input type='text'/><br/>
+					<input type='submit'/>
+					</form>
+				</InnerDiv>
+			</OuterDiv>
 		)
 	}
 }
 
 export default NewUserForm;
+
+const InnerDiv = styled.div`
+	margin: auto;
+	width: 40%;
+	padding: 20px;
+	border-style: solid;
+	border-width: 1px;
+	box-shadow: 10px 10px;
+`
+
+const OuterDiv = styled.div`
+	height: 200px;
+	display: flex;
+	justify-content: center;
+	align-items: center;
+`
