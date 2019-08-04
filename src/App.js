31b4df440c08687f8cbecc52a6c8c5b3af372c55
Matt Readout <matt.readout@gmail.com>
2019-08-04 19:24:20 -0400

    add navbar

--- src/App.js
+++ src/App.js
@@ -1,11 +1,21 @@
 import React from 'react';
 import FormContainer from './containers/FormContainer'
 import './App.css';
+import Navbar from 'react-bootstrap/Navbar'
+import Nav from 'react-bootstrap/Nav'
 
 class App extends React.Component {
 	render () {
 		return (
-	    <div className="App">
+	    <div className="Main">
+				<Navbar bg='dark' variant='dark'>
+					<Navbar.Brand href='/'>TaskTracker</Navbar.Brand>
+					<Nav>
+						<Nav.Link>Login</Nav.Link>
+						<Nav.Link>Something</Nav.Link>
+						<Nav.Link>Something Else</Nav.Link>
+					</Nav>
+				</Navbar>
 				<FormContainer />
 	    </div>
 	  );
