c6ada1bbb2be3f94c8cdab77feb89ab98156e2e7
Matt Readout <matt.readout@gmail.com>
2019-08-25 22:36:49 -0400

    add route to login form

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import FormContainer from './containers/FormContainer'
+import LoginForm from './components/LoginForm'
 import './App.css';
 import Navbar from 'react-bootstrap/Navbar'
 import Nav from 'react-bootstrap/Nav'
@@ -13,13 +14,16 @@ class App extends React.Component {
 					<Navbar bg='dark' variant='dark'>
 						<Navbar.Brand href='/'>TaskTracker</Navbar.Brand>
 						<Nav>
-							<Nav.Link>Login</Nav.Link>
+							<Nav.Link
+								as={ Link }
+								to='/login'>Login</Nav.Link>
 							<Nav.Link>Something</Nav.Link>
 							<Nav.Link>Something Else</Nav.Link>
 						</Nav>
 					</Navbar>
 					<div>
 						<Route exact path='/' render={() => <FormContainer />}/>
+						<Route path='/login' render={() => <LoginForm />}/>
 					</div>
 				</Router>
 	    </div>
