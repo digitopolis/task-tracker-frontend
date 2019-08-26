4589524eba6f091e9c404193521e02528a60193b
Matt Readout <matt.readout@gmail.com>
2019-08-25 22:34:38 -0400

    add react router and one route

--- src/App.js
+++ src/App.js
@@ -3,20 +3,25 @@ import FormContainer from './containers/FormContainer'
 import './App.css';
 import Navbar from 'react-bootstrap/Navbar'
 import Nav from 'react-bootstrap/Nav'
+import { BrowserRouter as Router, Route, Link } from 'react-router-dom'
 
 class App extends React.Component {
 	render () {
 		return (
 	    <div className="Main">
-				<Navbar bg='dark' variant='dark'>
-					<Navbar.Brand href='/'>TaskTracker</Navbar.Brand>
-					<Nav>
-						<Nav.Link>Login</Nav.Link>
-						<Nav.Link>Something</Nav.Link>
-						<Nav.Link>Something Else</Nav.Link>
-					</Nav>
-				</Navbar>
-				<FormContainer />
+				<Router>
+					<Navbar bg='dark' variant='dark'>
+						<Navbar.Brand href='/'>TaskTracker</Navbar.Brand>
+						<Nav>
+							<Nav.Link>Login</Nav.Link>
+							<Nav.Link>Something</Nav.Link>
+							<Nav.Link>Something Else</Nav.Link>
+						</Nav>
+					</Navbar>
+					<div>
+						<Route exact path='/' render={() => <FormContainer />}/>
+					</div>
+				</Router>
 	    </div>
 	  );
 	}
---
+++ src/components/LoginForm.js
@@ -0,0 +1,12 @@
+import React from 'react'
+// import PropTypes from 'prop-types'
+
+class LoginForm extends React.Component {
+	render () {
+		return (
+			<h1>Login Form!</h1>
+		)
+	}
+}
+
+export default LoginForm;
